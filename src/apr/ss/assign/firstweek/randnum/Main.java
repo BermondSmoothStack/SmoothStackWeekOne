1b35cc154ee9a149287ca7212c980995ce7996b8
Bermond Jon Batistiana <berjondev@gmail.com>
2021-03-09 13:41:24 -0800

    Comment from debug inverted

--- src/apr/ss/assign/firstweek/randnum/Main.java
+++ src/apr/ss/assign/firstweek/randnum/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         GenerateNumber number = new GenerateNumber();
-//        GuessNumber game = new GuessNumber(number.randomNumber);
-        GuessNumber game = new GuessNumber(10);
+        GuessNumber game = new GuessNumber(number.randomNumber);
+//        GuessNumber game = new GuessNumber(10);
         game.printGameMessage();
 
     }
