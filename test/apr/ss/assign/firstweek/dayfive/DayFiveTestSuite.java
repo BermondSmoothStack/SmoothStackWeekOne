42a85c116759eea1873bead07952893b9931633b
Bermond Jon Batistiana <berjondev@gmail.com>
2021-03-13 17:37:26 -0800

    DateAPI TestSuite

--- test/apr/ss/assign/firstweek/dayfive/DayFiveTestSuite.java
+++ test/apr/ss/assign/firstweek/dayfive/DayFiveTestSuite.java
@@ -3,11 +3,12 @@ package apr.ss.assign.firstweek.dayfive;
 import apr.ss.assign.firstweek.dayfive.arrayfilter.ArrayFilterTestSuite;
 import apr.ss.assign.firstweek.dayfive.arrayjoin.ArrayJoinTestSuite;
 import apr.ss.assign.firstweek.dayfive.basiclambdas.BasicLambdasTestSuite;
+import apr.ss.assign.firstweek.dayfive.dateapi.DateAPIHelperTestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({BasicLambdasTestSuite.class, ArrayFilterTestSuite.class, ArrayJoinTestSuite.class})
+@SuiteClasses({BasicLambdasTestSuite.class, ArrayFilterTestSuite.class, ArrayJoinTestSuite.class, DateAPIHelperTestSuite.class})
 public class DayFiveTestSuite {
 }
---
+++ test/apr/ss/assign/firstweek/dayfive/dateapi/DateAPIHelperTestSuite.java
@@ -0,0 +1,11 @@
+package apr.ss.assign.firstweek.dayfive.dateapi;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses(DateAPIHelperMethodsTest.class)
+public class DateAPIHelperTestSuite {
+}
