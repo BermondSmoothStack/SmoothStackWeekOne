04c41796bf52bf7f6924faa7c752435e685b4f89
Bermond Jon Batistiana <berjondev@gmail.com>
2021-03-15 14:04:37 -0700

    Ones Test Suite

--- test/apr/ss/assign/firstweek/weekend/WeekendTestSuite.java
+++ test/apr/ss/assign/firstweek/weekend/WeekendTestSuite.java
@@ -4,12 +4,13 @@ package apr.ss.assign.firstweek.weekend;
 import apr.ss.assign.firstweek.weekend.doubleint.DoubleIntTestSuite;
 import apr.ss.assign.firstweek.weekend.lbdop.OperationTestSuite;
 import apr.ss.assign.firstweek.weekend.nox.NoXTestSuite;
+import apr.ss.assign.firstweek.weekend.ones.OnesTestSuite;
 import apr.ss.assign.firstweek.weekend.recursion.RecursionTestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({DoubleIntTestSuite.class, OperationTestSuite.class, NoXTestSuite.class, RecursionTestSuite.class})
+@SuiteClasses({DoubleIntTestSuite.class, OperationTestSuite.class, NoXTestSuite.class, RecursionTestSuite.class, OnesTestSuite.class})
 public class WeekendTestSuite {
 }
---
+++ test/apr/ss/assign/firstweek/weekend/ones/OnesTestSuite.java
@@ -0,0 +1,12 @@
+package apr.ss.assign.firstweek.weekend.ones;
+
+
+import apr.ss.assign.firstweek.weekend.lbdop.OperationTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({OnesDigitTest.class})
+public class OnesTestSuite {
+}
